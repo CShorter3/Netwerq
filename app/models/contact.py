bc0681142aca180e9a248fb2e727cf7913644028
Calid Shorter <CalidShorter3@gmail.com>
2025-02-25 14:57:03 -0500

    adds a contact instance method that converts contact objects into python dictionaries

--- app/models/contact.py
+++ app/models/contact.py
@@ -24,3 +24,19 @@ class Contact(db.Model):
         """Updates last_contacted timestamp in EST time."""
         self.last_contacted = datetime.now(ZoneInfo("America/New_York"))
         db.session.commit()
+
+    def to_dict(self):
+        """Returns a dictionary representation of a contact, easily interpreted by the front end."""
+        return {
+            "id": self.id,
+            "relation_type": self.relation_type,
+            "city": self.city,
+            "state": self.state,
+            "number": self.number,
+            "title": self.title,
+            "company": self.company,
+            "last_contacted": self.last_contacted.isoformat() if self.last_contacted else "Never Contacted",
+            "init_meeting_note": self.init_meeting_note,
+            "distinct_memory_note": self.distinct_memory_note,
+            "user_id": self.user_id,
+        }  
