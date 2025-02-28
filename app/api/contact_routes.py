cec7239112a9622b28da9d7acaa2ed17633fca3f
Calid Shorter <CalidShorter3@gmail.com>
2025-02-27 23:05:19 -0500

    adds get all user contact api/contacts/ route and corrects blueprint file path error

--- app/api/contact_routes.py
+++ app/api/contact_routes.py
@@ -5,3 +5,14 @@ from app.forms import ContactForm
 
 contact_routes = Blueprint('contacts', __name__)
 
+# GET all of the current user's contacts
+@contact_routes.route('/', methods=['GET'])
+@login_required
+def get_contacts():
+    """
+    returns all of the user's contacts a list
+    """
+    print(f"User {current_user.id} is requesting contacts", file=sys.stderr, flush=True)
+    contacts = Contact.query.filter(Contact.user_id == current_user.id).all()
+
+    return {'contacts': [contact.to_dict() for contact in contacts]}, 200
