b02a37ffbf7ae82fcb03fe2306887de9feaba702
Calid Shorter <CalidShorter3@gmail.com>
2025-02-27 21:09:46 -0500

    creates add contact flask jwt form

---
+++ app/forms/create_contact_form.py
@@ -0,0 +1,61 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
+
+class ContactForm(FlaskForm):
+    """
+    Form for creating and updating contacts
+    """
+    first_name = StringField('First Name', validators=[
+        DataRequired(message="First name is required"),
+        Length(min=1, max=30, message="First name must be between 1 and 30 characters")
+    ])
+    
+    last_name = StringField('Last Name', validators=[
+        DataRequired(message="Last name is required"),
+        Length(min=1, max=30, message="Last name must be between 1 and 30 characters")
+    ])
+    
+    relation_type = SelectField('Relationship Type', validators=[
+        DataRequired(message="Relationship type is required")
+    ], choices=[
+        ('mentor', 'Mentor'),
+        ('peer', 'Peer'),
+        ('mentee', 'Mentee'),
+        ('recruiter', 'Recruiter')
+    ])
+    
+    city = StringField('City', validators=[
+        Optional(),
+        Length(max=35, message="City name must be less than 35 characters")
+    ])
+    
+    state = StringField('State', validators=[
+        Optional(),
+        Length(max=35, message="State name must be less than 35 characters")
+    ])
+    
+    number = StringField('Phone Number', validators=[
+        Optional(),
+        Length(max=20, message="Phone number must be less than 20 characters")
+    ])
+    
+    job_title = StringField('Job Title', validators=[
+        Optional(),
+        Length(max=50, message="Job title must be less than 50 characters")
+    ])
+    
+    company = StringField('Company', validators=[
+        Optional(),
+        Length(max=50, message="Company name must be less than 50 characters")
+    ])
+    
+    init_meeting_note = TextAreaField('Initial Meeting Note', validators=[
+        DataRequired(message="Initial meeting note is required"),
+        Length(min=1, max=300, message="Initial meeting note must be between 1 and 300 characters")
+    ])
+    
+    distinct_memory_note = TextAreaField('Distinctive Memory Note', validators=[
+        DataRequired(message="Distinctive memory note is required"),
+        Length(min=1, max=300, message="Distinctive memory note must be between 1 and 300 characters")
+    ])
