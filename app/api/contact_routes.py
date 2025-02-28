81162fc464460258eb51c3631896aebb9e618e5b
Calid Shorter <CalidShorter3@gmail.com>
2025-02-27 23:52:32 -0500

    adds route to get a user contact by id

--- app/api/contact_routes.py
+++ app/api/contact_routes.py
@@ -5,13 +5,31 @@ from app.forms import ContactForm
 
 contact_routes = Blueprint('contacts', __name__)
 
-# GET all of the current user's contacts
+# GET users' contacts
 @contact_routes.route('/', methods=['GET'])
 @login_required
 def get_contacts():
     """
-    returns all of the user's contacts a list
+    Returns all of the user's contacts a list
     """
     contacts = Contact.query.filter(Contact.user_id == current_user.id).all()
-
     return {'contacts': [contact.to_dict() for contact in contacts]}, 200
+
+# GET a user's contact
+@contact_routes('/<int:id>', methods=['GET'])
+@login_required
+def get_connection(id):
+    """
+    Returns a specific user's contact by id
+    """
+    contact = Contact.query.get(id)
+    
+    # Ensure contact exists
+    if not contact:
+        return {'errors': {'message': 'Contact not found'}}, 404
+        
+    # Ensure contact belongs to session user
+    if contact.user_id != current_user.id:
+        return {'errors': {'message': 'Unauthorized'}}, 403
+        
+    return contact.to_dict(), 200
