4ad865cd6dea9e6a3d456f8e485b0c5b7fe8a72f
Calid Shorter <CalidShorter3@gmail.com>
2025-02-28 01:31:21 -0500

    re-orders route order of contact routes

--- app/api/contact_routes.py
+++ app/api/contact_routes.py
@@ -19,29 +19,6 @@ def get_contacts():
     return jsonify({'contacts': [contact.to_dict() for contact in contacts]}), 200
 
 
-# GET a session user's contact by id
-@contact_routes.route('/<int:id>', methods=['GET'])
-@login_required
-def get_connection(id):
-    """
-    Return a specific contact
-    """
-    #print(f"Fetching contact with ID: {id}")
-
-    contact = Contact.query.get(id)
-    #print("Contact:", contact)
-
-    # Ensure contact exists
-    if not contact:
-        return jsonify({'errors': {'message': 'Contact not found'}}), 404
-        
-    # Ensure contact belongs to session user
-    if contact.user_id != current_user.id:
-        return jsonify({'errors': {'message': 'Unauthorized'}}), 403
-        
-    return jsonify(contact.to_dict()), 200
-
-
 # POST a new contact
 @contact_routes.route('', methods=['POST'])
 @login_required
@@ -73,3 +50,28 @@ def create_contact():
         return jsonify(contact.to_dict()), 200
     
     return jsonify({'errors': form.errors}), 400
+
+
+# GET a session user's contact by id
+@contact_routes.route('/<int:id>', methods=['GET'])
+@login_required
+def get_connection(id):
+    """
+    Return a specific contact
+    """
+    #print(f"Fetching contact with ID: {id}")
+
+    contact = Contact.query.get(id)
+    #print("Contact:", contact)
+
+    # Ensure contact exists
+    if not contact:
+        return jsonify({'errors': {'message': 'Contact not found'}}), 404
+        
+    # Ensure contact belongs to session user
+    if contact.user_id != current_user.id:
+        return jsonify({'errors': {'message': 'Unauthorized'}}), 403
+        
+    return jsonify(contact.to_dict()), 200
+
+
