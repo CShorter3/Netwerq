a172cb6b15ec4c2424597cf055cf0e391f010994
Calid Shorter <CalidShorter3@gmail.com>
2025-03-01 13:23:46 -0500

    adds csrf extraction utility

--- react-vite/src/redux/contact.js
+++ react-vite/src/redux/contact.js
@@ -1,10 +1,36 @@
+/* Action Constants */
 const ADD_CONTACT = "contact/addContact";
 
+/* Action Creators */
 const addContact = (contact) => ({
     type: ADD_CONTACT,
     payload: contact
 });
 
+/* Csrf Extraction Utility */
+const getCsrfToken = () => {
+    return document.cookie
+      .split('; ')
+      .find(row => row.startsWith('csrf_token='))
+      ?.split('=')[1];
+};
+  
+
+/* Thunk Actions */
+export const saveContact = (contactData) => async (dispatch) => {
+    console.log("*****INSIDE SAVE CONTACT THUNK!*****")
+    const response = await fetch("/api/contacts", {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(contactData)
+    });
+    console.log("returned contact date response: ", response);
+}
+
+
+/* Contact slice of root reducer */
 const initialState = { user: null };
 
 function contactReducer(state = initialState, action){
