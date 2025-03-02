92ea1598fc642e14bacdb60dc9a9dc6098c38f38
Calid Shorter <CalidShorter3@gmail.com>
2025-03-01 19:49:23 -0500

    adds update contact reducer case to contact reducer

--- react-vite/src/redux/contact.js
+++ react-vite/src/redux/contact.js
@@ -96,6 +96,14 @@ function contactReducer(state = initialState, action){
                 contacts: [...state.contacts, action.payload],
                 currentContact: action.payload
             };
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => 
+                    contact.id === action.payload.id ? action.payload : contact
+                ),
+                currentContact: action.payload
+            };
         default:
             return state;
     }
