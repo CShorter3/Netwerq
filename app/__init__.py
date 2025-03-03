9209b2f0deb4d80da3f222052fff2c2d70398111
Calid Shorter <CalidShorter3@gmail.com>
2025-03-03 10:33:04 -0500

    prepare to push

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.contact_routes import contact_routes
 from .api.opportunity_routes import opportunity_routes
+from .api.contact_routes import contact_routes
 from .seeds import seed_commands
 from .config import Config
 
--- app/api/opportunity_routes.py
+++ app/api/opportunity_routes.py
@@ -7,12 +7,12 @@ from datetime import datetime, date, timedelta
 opportunity_routes = Blueprint('opportunities', __name__)
 
 
-# GET all session user's opportunities
-# @opportunity_routes.route('/', methods=['GET'])
-# @login_required
-# def get_opportunities():
-#     """
-#     Return all opportunities for the current user
-#     """
-#     opportunities = Opportunity.query.filter(Opportunity.user_id == current_user.id).all()
-#     return jsonify({'opportunities': [opportunity.to_dict() for opportunity in opportunities]}), 200
+#GET all session user's opportunities
+@opportunity_routes.route('/', methods=['GET'])
+@login_required
+def get_opportunities():
+    """
+    Return all opportunities for the current user
+    """
+    opportunities = Opportunity.query.filter(Opportunity.user_id == current_user.id).all()
+    return jsonify({'opportunities': [opportunity.to_dict() for opportunity in opportunities]}), 200
