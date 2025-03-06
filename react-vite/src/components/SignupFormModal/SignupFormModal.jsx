5231198b0dcd6c447f460b8447ac3e94cef3b4b3
Calid Shorter <CalidShorter3@gmail.com>
2025-03-06 08:40:29 -0500

    styles the signup modal

--- react-vite/src/components/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,3 +1,97 @@
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { useModal } from "../../context/Modal";
+// import { thunkSignup } from "../../redux/session";
+// import "./SignupForm.css";
+
+// function SignupFormModal() {
+//   const dispatch = useDispatch();
+//   const [email, setEmail] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+//   const [errors, setErrors] = useState({});
+//   const { closeModal } = useModal();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     if (password !== confirmPassword) {
+//       return setErrors({
+//         confirmPassword:
+//           "Confirm Password field must be the same as the Password field",
+//       });
+//     }
+
+//     const serverResponse = await dispatch(
+//       thunkSignup({
+//         email,
+//         username,
+//         password,
+//       })
+//     );
+
+//     if (serverResponse) {
+//       setErrors(serverResponse);
+//     } else {
+//       closeModal();
+//     }
+//   };
+
+//   return (
+//     <>
+//       <h1>Sign Up</h1>
+//       {errors.server && <p>{errors.server}</p>}
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Email
+//           <input
+//             type="text"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.email && <p>{errors.email}</p>}
+//         <label>
+//           Username
+//           <input
+//             type="text"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.username && <p>{errors.username}</p>}
+//         <label>
+//           Password
+//           <input
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.password && <p>{errors.password}</p>}
+//         <label>
+//           Confirm Password
+//           <input
+//             type="password"
+//             value={confirmPassword}
+//             onChange={(e) => setConfirmPassword(e.target.value)}
+//             required
+//           />
+//         </label>
+//         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+//         <button type="submit">Sign Up</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default SignupFormModal;
+
+
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -15,14 +109,13 @@ function SignupFormModal() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (password !== confirmPassword) {
       return setErrors({
         confirmPassword:
           "Confirm Password field must be the same as the Password field",
       });
     }
-
+    
     const serverResponse = await dispatch(
       thunkSignup({
         email,
@@ -30,7 +123,7 @@ function SignupFormModal() {
         password,
       })
     );
-
+    
     if (serverResponse) {
       setErrors(serverResponse);
     } else {
@@ -39,53 +132,76 @@ function SignupFormModal() {
   };
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      {errors.server && <p>{errors.server}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
-      </form>
-    </>
+    <div className="signup-modal">
+      {/* Header */}
+      <div className="signup-modal-header">
+        <h2>Join Netwerq</h2>
+        <p>Create an account to build your professional network</p>
+      </div>
+      
+      {/* Content */}
+      <div className="signup-modal-content">
+        {errors.server && <p className="error-message server-error">{errors.server}</p>}
+        
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            {errors.email && <p className="error-message">{errors.email}</p>}
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            {errors.username && <p className="error-message">{errors.username}</p>}
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {errors.password && <p className="error-message">{errors.password}</p>}
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              id="confirmPassword"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
+          </div>
+          
+          <button type="submit" className="submit-button">Sign Up</button>
+        </form>
+        
+        <div className="login-prompt">
+          <p>
+            Already have an account? <a href="#">Log in</a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
