66586aeb12b1d0c7181a7065abef679fa9ff00e3
Calid Shorter <CalidShorter3@gmail.com>
2025-03-06 15:44:17 -0500

    adds proper signup form validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, ValidationError, Length, EqualTo
 from app.models import User
 
 
@@ -21,7 +21,49 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username', validators=[
+            DataRequired(message="Username is required"),
+            Length(min=3, max=40, message="Username must be between 3 and 40 characters"),
+            username_exists
+    ])
+
+    email = StringField(
+        'email', validators=[
+            DataRequired(message="Email is required"),
+            Email(message="Please provide a valid email"),
+            user_exists
+    ])
+    password = StringField(
+        'password', validators=[
+            DataRequired(message="Password is required"),
+            Length(min=6, message="Password must be at least 6 characters long")
+    ])
+
+    confirm_password = PasswordField(
+        'confirm_password', validators=[
+            DataRequired(message="Please confirm your password"),
+            EqualTo('password', message="Passwords must match")
+    ])
+
+    # Optional fields
+    first_name = StringField(
+        'first_name', validators=[
+            Length(max=30, message="First name must be less than 30 characters")
+    ])
+    
+    last_name = StringField(
+        'last_name', validators=[
+            Length(max=30, message="Last name must be less than 30 characters")
+    ])
+    
+    city = StringField(
+        'city', validators=[
+            Length(max=35, message="City must be less than 35 characters")
+    ])
+    
+    state = StringField(
+        'state', validators=[
+            Length(max=35, message="State must be less than 35 characters")
+    ])
