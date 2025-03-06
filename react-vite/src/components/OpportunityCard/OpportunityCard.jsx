9f3bd698b76b383a09636ddeb229c4191d4a7c35
Calid Shorter <CalidShorter3@gmail.com>
2025-03-06 00:50:10 -0500

    completes the opportunity card before (without styling)

--- react-vite/src/components/OpportunityCard/OpportunityCard.jsx
+++ react-vite/src/components/OpportunityCard/OpportunityCard.jsx
@@ -1,37 +1,43 @@
 import { Calendar, Edit2 } from 'lucide-react';
 
-function OpportunityCard(){
+function OpportunityCard({ opportunity, onEdit }){
+
+    // Unitility function: Format date mm/dd/yyyy 
+    function formatDate(dateString) {
+        if (!dateString) return "N/A";
+        const date = new Date(dateString);
+        return date.toLocaleDateString();
+      }
 
     return (
         <div className="opportunity-card">
-            <div className="opportunity-icon"></div>
+            <div className="opportunity-icon">{opportunity.icon}</div>
             <div className="opportunity-details">
                 <div className="opportunity-header">
-                <h3>Phone Call</h3>
-                <span className="Active">
-                    Active
+                <h3>{opportunity.title}</h3>
+                <span className={`opportunity-status status-${opportunity.status.toLowerCase()}`}>
+                    {opportunity.status}
                 </span>
                 </div>
-                <p className="opportunity-description">Casual low pressure convo</p>
+                <p className="opportunity-description">{opportunity.description}</p>
                 <div className="opportunity-subinfo">
                 <span className="opportunity-occurrence">
                     <Calendar size={14} />
-                    {"Bi-montly"} • Next: {"11/07/2024"}
+                    {opportunity.occurrence} • Next: {formatDate(opportunity.next_date)}
                 </span>
                 </div>
             </div>
-        <div className="opportunity-actions">
-            <button 
-                className="edit-opportunity-button"
-                //   onClick={() => onEdit(opportunity)}
-                aria-label="Edit opportunity"
-            >
-            <Edit2 size={16} />
-            </button>
+            <div className="opportunity-actions">
+                <button 
+                    className="edit-opportunity-button"
+                    onClick={() => onEdit(opportunity)}
+                    aria-label="Edit opportunity"
+                >
+                    <Edit2 size={16} />
+                </button>
+            </div>
         </div>
-        </div>
-        )
-
-    }
+    );
+}
 
 export default OpportunityCard;
