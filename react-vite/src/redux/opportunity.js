9651264278a749d2511520ec753a389b67243653
Calid Shorter <CalidShorter3@gmail.com>
2025-03-06 02:06:41 -0500

    implements update opportunity redux thunk action and reducer logic

--- react-vite/src/redux/opportunity.js
+++ react-vite/src/redux/opportunity.js
@@ -1,6 +1,7 @@
 /* Action Constants */
 const FETCH_OPPORTUNITIES = "opportunity/fetchOpportunities";
 const ADD_OPPORTUNITY = "opportunity/addOpportunity";
+const UPDATE_OPPORTUNITY = "opportunity/updateOpportunity";
 
 
 /* Action Creators */
@@ -13,6 +14,11 @@ const addOpportunity = (opportunity) => ({
     type: ADD_OPPORTUNITY,
     payload: opportunity
 });
+
+const updateOpportunity = (opportunity) => ({
+    type: UPDATE_OPPORTUNITY,
+    payload: opportunity
+  });
 
 
 /* CSRF token extraction utility */
@@ -53,14 +59,15 @@ console.log("***** INSIDE FETCH OPPS THUNK! *****");
 
 
 export const createOpportunityThunk = (contactId, opportunityData) => async (dispatch) => {
-    const csrfToken = getCsrfToken();
+    //const csrfToken = getCsrfToken();
+    console.log("***** INSIDE CREATE OPPS THUNK! *****");
 
     try {
         const response = await fetch(`/api/opportunities/contact/${contactId}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "X-CSRFToken": opportunityData.csrf_token || csrfToken
+            "X-CSRFToken": opportunityData.csrf_token || getCsrfToken()
         },
         credentials: 'include',
         body: JSON.stringify(opportunityData)
@@ -81,6 +88,36 @@ export const createOpportunityThunk = (contactId, opportunityData) => async (dis
 };
 
 
+export const updateOpportunityThunk = (opportunityId, opportunityData) => async (dispatch) => {
+    //const csrfToken = getCsrfToken();
+    console.log("***** INSIDE UPDATE OPPS THUNK! *****");
+
+    try {
+      const response = await fetch(`/api/opportunities/${opportunityId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": opportunityData.csrf_token || getCsrfToken()
+        },
+        credentials: 'include',
+        body: JSON.stringify(opportunityData)
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        return { errors: errorData.errors || "Failed to update opportunity" };
+      }
+  
+      const data = await response.json();
+      dispatch(updateOpportunity(data));
+      return data;
+    } catch (error) {
+      console.error("Error updating opportunity:", error);
+      return { errors: error.toString() };
+    }
+};
+
+
 /* Opportunity reducer */
 const initialState = {
     opportunities: []
@@ -90,14 +127,22 @@ function opportunityReducer(state = initialState, action) {
     switch (action.type) {
         case FETCH_OPPORTUNITIES:
             return {
-            ...state,
-            opportunities: action.payload
+                ...state,
+                opportunities: action.payload
         };
         case ADD_OPPORTUNITY:
             return {
-            ...state,
-            opportunities: [...state.opportunities, action.payload]
+                ...state,
+                opportunities: [...state.opportunities, action.payload]
         };
+        case UPDATE_OPPORTUNITY:
+            return {
+                ...state,
+                opportunities: state.opportunities.map(opportunity => 
+                opportunity.id === action.payload.id ? action.payload : opportunity
+                )
+            };
+      
         default:
             return state;
     }
